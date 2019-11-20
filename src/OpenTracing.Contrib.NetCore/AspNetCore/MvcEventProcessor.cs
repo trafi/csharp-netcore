48d680d5166603108e92d38c2064353379924fee
Donatas Mačiūnas <donatas@trafi.com>
2019-11-20 10:42:14 +0200

    Also check property names as defined in aspnetcore 3

--- src/OpenTracing.Contrib.NetCore/AspNetCore/MvcEventProcessor.cs
+++ src/OpenTracing.Contrib.NetCore/AspNetCore/MvcEventProcessor.cs
@@ -18,10 +18,10 @@ namespace OpenTracing.Contrib.NetCore.AspNetCore
 
         private const string ResultTagType = "result.type";
 
-        private static readonly PropertyFetcher _beforeAction_httpContextFetcher = new PropertyFetcher("httpContext");
-        private static readonly PropertyFetcher _beforeAction_ActionDescriptorFetcher = new PropertyFetcher("actionDescriptor");
-        private static readonly PropertyFetcher _beforeActionResult_actionContextFetcher = new PropertyFetcher("actionContext");
-        private static readonly PropertyFetcher _beforeActionResult_ResultFetcher = new PropertyFetcher("result");
+        private static PropertyFetcher _beforeAction_httpContextFetcher;
+        private static PropertyFetcher _beforeAction_ActionDescriptorFetcher;
+        private static PropertyFetcher _beforeActionResult_actionContextFetcher;
+        private static PropertyFetcher _beforeActionResult_ResultFetcher;
 
         private readonly ITracer _tracer;
         private readonly ILogger _logger;
@@ -33,7 +33,7 @@ namespace OpenTracing.Contrib.NetCore.AspNetCore
             _ignorePatterns = ignorePatterns;
             _tracer = tracer ?? throw new ArgumentNullException(nameof(tracer));
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            _options = options ?? throw new ArgumentNullException(nameof(options));;
+            _options = options ?? throw new ArgumentNullException(nameof(options));
         }
 
         public bool ProcessEvent(string eventName, object arg)
@@ -42,7 +42,7 @@ namespace OpenTracing.Contrib.NetCore.AspNetCore
             {
                 case "Microsoft.AspNetCore.Mvc.BeforeAction":
                 {
-                    var httpContext = (HttpContext)_beforeAction_httpContextFetcher.Fetch(arg);
+                    var httpContext = GetHttpContext(arg);
 
                     if (ShouldIgnore(httpContext))
                     {
@@ -53,7 +53,7 @@ namespace OpenTracing.Contrib.NetCore.AspNetCore
                         // NOTE: This event is the start of the action pipeline. The action has been selected, the route
                         //       has been selected but no filters have run and model binding hasn't occured.
 
-                        var actionDescriptor = (ActionDescriptor)_beforeAction_ActionDescriptorFetcher.Fetch(arg);
+                        var actionDescriptor = GetActionDescriptor(arg);
                         var controllerActionDescriptor = actionDescriptor as ControllerActionDescriptor;
 
                         string operationName = _options.ActionOperationNameResolver(actionDescriptor);
@@ -77,7 +77,7 @@ namespace OpenTracing.Contrib.NetCore.AspNetCore
 
                 case "Microsoft.AspNetCore.Mvc.BeforeActionResult":
                 {
-                    var httpContext = ((ActionContext) _beforeActionResult_actionContextFetcher.Fetch(arg)).HttpContext;
+                    var httpContext = GetActionContext(arg).HttpContext;
 
                     if (ShouldIgnore(httpContext))
                     {
@@ -88,7 +88,7 @@ namespace OpenTracing.Contrib.NetCore.AspNetCore
                         // NOTE: This event is the start of the result pipeline. The action has been executed, but
                         //       we haven't yet determined which view (if any) will handle the request
 
-                        object result = _beforeActionResult_ResultFetcher.Fetch(arg);
+                        object result = GetResult(arg);
                         string resultType = result.GetType().Name;
 
                         string operationName = _options.ResultOperationNameResolver(result);
@@ -109,13 +109,98 @@ namespace OpenTracing.Contrib.NetCore.AspNetCore
                     }
                     return true;
 
-                default: return false;
+                default:
+                    return false;
             }
         }
 
         private bool ShouldIgnore(HttpContext httpContext)
         {
             return _ignorePatterns.Any(ignore => ignore(httpContext));
+        }
+
+        private HttpContext GetHttpContext(object obj)
+        {
+            if (_beforeAction_httpContextFetcher == null)
+            {
+                if (PropertyFetcher.HasProperty(obj, "httpContext"))
+                {
+                    _beforeAction_httpContextFetcher = new PropertyFetcher("httpContext");
+                }
+                else if (PropertyFetcher.HasProperty(obj, "HttpContext"))
+                {
+                    _beforeAction_httpContextFetcher = new PropertyFetcher("HttpContext");
+                }
+                else
+                {
+                    throw new ArgumentException("provided object doesn't have expected property 'HttpContext'");
+                }
+            }
+
+            return (HttpContext)_beforeAction_httpContextFetcher.Fetch(obj);
+        }
+
+        private ActionDescriptor GetActionDescriptor(object obj)
+        {
+            if (_beforeAction_ActionDescriptorFetcher == null)
+            {
+                if (PropertyFetcher.HasProperty(obj, "actionDescriptor"))
+                {
+                    _beforeAction_ActionDescriptorFetcher = new PropertyFetcher("actionDescriptor");
+                }
+                else if (PropertyFetcher.HasProperty(obj, "ActionDescriptor"))
+                {
+                    _beforeAction_ActionDescriptorFetcher = new PropertyFetcher("ActionDescriptor");
+                }
+                else
+                {
+                    throw new ArgumentException("provided object doesn't have expected property 'ActionDescriptor'");
+                }
+            }
+
+            return (ActionDescriptor)_beforeAction_ActionDescriptorFetcher.Fetch(obj);
+        }
+
+        private ActionContext GetActionContext(object obj)
+        {
+            if (_beforeActionResult_actionContextFetcher == null)
+            {
+                if (PropertyFetcher.HasProperty(obj, "actionContext"))
+                {
+                    _beforeActionResult_actionContextFetcher = new PropertyFetcher("actionContext");
+                }
+                else if (PropertyFetcher.HasProperty(obj, "ActionContext"))
+                {
+                    _beforeActionResult_actionContextFetcher = new PropertyFetcher("ActionContext");
+                }
+                else
+                {
+                    throw new ArgumentException("provided object doesn't have expected property 'ActionContext'");
+                }
+            }
+
+            return (ActionContext)_beforeActionResult_actionContextFetcher.Fetch(obj);
+        }
+
+        private object GetResult(object obj)
+        {
+            if (_beforeActionResult_ResultFetcher == null)
+            {
+                if (PropertyFetcher.HasProperty(obj, "result"))
+                {
+                    _beforeActionResult_ResultFetcher = new PropertyFetcher("result");
+                }
+                else if (PropertyFetcher.HasProperty(obj, "Result"))
+                {
+                    _beforeActionResult_ResultFetcher = new PropertyFetcher("Result");
+                }
+                else
+                {
+                    throw new ArgumentException("provided object doesn't have expected property 'Result'");
+                }
+            }
+
+            return _beforeActionResult_ResultFetcher.Fetch(obj);
         }
     }
 }
